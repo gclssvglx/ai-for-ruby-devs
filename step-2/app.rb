4dc9a4cea0348fd2b7fe6be4e4b2f0a4ea86e9ea
Graham Charles Lewis <grahamcharleslewis@gmail.com>
2023-09-14 14:03:49 +0100

    Update Step 2

--- step-2/app.rb
+++ step-2/app.rb
@@ -11,18 +11,13 @@ end
 post "/" do
   client = OpenAI::Client.new(access_token: ENV["OPEN_AI_ACCESS_TOKEN"])
 
-  response = []
-  client.chat(
+  @reply = client.chat(
     parameters: {
       model: ENV["CHAT_MODEL"],
       messages: [{ role: "user", content: params[:prompt] }],
-      temperature: ENV["CHAT_TEMPERATURE"].to_f,
-      stream: proc do |chunk, _bytesize|
-        response << chunk.dig("choices", 0, "delta", "content")
-      end
+      temperature: ENV["CHAT_TEMPERATURE"].to_f
     }
-  )
+  ).dig("choices", 0, "message", "content")
 
-  @reply = response.join
   erb :index
 end
